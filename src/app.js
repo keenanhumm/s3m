c0aa0847798211dbc5ef6d4ecd5d053c6e1ba4ea
keenanhumm <keenan.humm@huskers.unl.edu>
2018-06-18 13:41:01 -0500

    eslint error fix

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
-
-console.log("Running app.js");
+// eslint-disable-next-line
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 const template = (
   <div>
     <h1>Welcome</h1>
   </div>
 );
-
-ReactDOM.render(
-  template,
-  document.getElementById("app")
-);
+// eslint-disable-next-line
+ReactDOM.render(template, document.getElementById('app'));
