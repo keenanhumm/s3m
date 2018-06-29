a295f90cd1b3aa876f92109f1adeebe7150dec6c
keenanhumm <keenan.humm@huskers.unl.edu>
2018-06-28 20:10:15 -0500

    possible bug fix

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const publicPath = path.join(__dirname, './public');
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/bundle.js'));
+});
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`server running at ${port}`);// eslint-disable-line
