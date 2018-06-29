56c99ffb011f24a3216edb1c784ada998590f3a7
keenanhumm <keenan.humm@huskers.unl.edu>
2018-06-28 19:28:47 -0500

    possible prod fix

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('./server/server');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, './public');
+// const key =
+//   'xsgMbsKGfvziD3z4GkeNYuQ4-3sWuPLDY2JnN9OPaA0.8EJyNKGsT3S5SsC5oJtcqm-N7O6_-lVrM9UnfNYm8zU';
+// const addressSSL = 'xsgMbsKGfvziD3z4GkeNYuQ4-3sWuPLDY2JnN9OPaA0';
+// app.get(`/.well-known/acme-challenge/${addressSSL}`, function(req, res) {
+//   res.send(key);
+// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+  console.log(`server running at ${port}`);
+});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const bodyParser = require('body-parser');
-const path = require('path');
 const cors = require('cors');
 const express = require('express');
 const graphql = require('graphql');
@@ -8,11 +7,6 @@ const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const fs = require('fs');
 const db = require('./db');
 const typeDefs = require('./typeDefs');
-
-
-const port = process.env.PORT || 3000;
-
-const publicPath = path.join(__dirname, '../public');
 
 const resolvers = require('./resolvers');
 
@@ -24,8 +18,4 @@ app.use(cors(), bodyParser.json());
 app.use('/graphql', graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-app.listen(port, () => console.log(`Server started on port ${port}`));// eslint-disable-line
+module.exports = app;
