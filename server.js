6e062c67a532f4deb5322fe580b42ceec9e949c7
keenanhumm <keenan.humm@huskers.unl.edu>
2018-07-04 21:35:02 -0500

    refactoring and small styling changes

--- server.js
+++ server.js
@@ -4,26 +4,28 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, './public');
+const publicPath = path.join(__dirname, '/public');
 // const key =
 //   'xsgMbsKGfvziD3z4GkeNYuQ4-3sWuPLDY2JnN9OPaA0.8EJyNKGsT3S5SsC5oJtcqm-N7O6_-lVrM9UnfNYm8zU';
 // const addressSSL = 'xsgMbsKGfvziD3z4GkeNYuQ4-3sWuPLDY2JnN9OPaA0';
 // app.get(`/.well-known/acme-challenge/${addressSSL}`, function(req, res) {
 //   res.send(key);
 // });
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+
+
+app.use('public', express.static(publicPath));
+app.get('*/bundle.js', (req, res) => {
+  res.sendFile(path.join(publicPath, '/bundle.js'));
 });
-app.get('/bundle.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/bundle.js'));
+app.get('*/style.css', (req, res) => {
+  res.sendFile(path.join(publicPath, '/style.css'));
 });
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/style.css'));
+app.get('*/favicon.png', (req, res) => {
+  res.sendFile(path.join(publicPath, '/favicon.png'));
 });
-app.get('/icon.png', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/icon.png'));
+app.get('*/', (req, res) => {
+  res.sendFile(path.join(publicPath, '/index.html'));
 });
-app.use('/public', express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`server running at ${port}`);// eslint-disable-line
 });
